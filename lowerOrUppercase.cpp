a93f6abee2a6d3af73438bd6c4f87f4894355a1a
Hasham Ahmed <159484100+ahmed-hashamm@users.noreply.github.com>
2025-02-16 18:15:16 +0500

    Update lowerOrUppercase.cpp

--- lowerOrUppercase.cpp
+++ lowerOrUppercase.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    char c;
-    
-    cout<<"Enter Character"<<endl;
-    cin>>c;
-   if(c>='A' && c<='Z')
-   {
-    cout<<"Upper Case";
-   }
-   else{
-    cout<<"Lower Case";
-   }
+    char c; // Variable to store the input character
 
-    return 0;
+    // Prompt the user to enter a character
+    cout << "Enter Character" << endl;
+    cin >> c; // Read the input character from the user
+    // Check if the character is an uppercase letter (A-Z)
+    if (c >= 'A' && c <= 'Z')
+    {
+        cout << "Upper Case"; // Print "Upper Case" if the character is uppercase
+    }
+    else
+    {
+        cout << "Lower Case"; // Print "Lower Case" if the character is lowercase or not a letter
+    }
+
+    return 0; // Indicate successful program termination
 }
